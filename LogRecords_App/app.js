1a249395a475b638f812d617dfc013d485e9d6ae
Antonio Diaz <antonio.diaz.02071998@gmail.com>
2021-04-02 14:58:29 -0500

    Finished Log records

--- LogRecords_App/app.js
+++ LogRecords_App/app.js
@@ -1,14 +1,14 @@
 // this is where I will use the modules to do the logging
 let obj = require("./input");
 
-//obj.input();
+obj.input();
 
-let product = function (a, b) {
-  debugger;
-  var pro = a * b;
-  debugger;
-  return pro;
-};
-debugger;
-let result = product(10, 3);
-debugger;
+// let product = function (a, b) {
+//   debugger;
+//   var pro = a * b;
+//   debugger;
+//   return pro;
+// };
+// debugger;
+// let result = product(10, 3);
+// debugger;
